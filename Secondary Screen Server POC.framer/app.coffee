8f99e4d3ba838f8a5897857ad265e78f236b7285
Jordan Stapinski <jstapins@andrew.cmu.edu>
2018-07-09 10:29:21 -0400

    Fixed style for second screen POC and completed POC construction

--- Secondary Screen Server POC.framer/app.coffee
+++ Secondary Screen Server POC.framer/app.coffee
@@ -1,7 +1,11 @@
+# Generic helper function that brings the desired layer to
+# the forefront 
 screen_to_show = (screen) ->
 	screen.x = 0
 	screen.bringToFront()
 	
+# Listen for a websocket event at the server specified at
+# the bottom of index.html 
 ws.onmessage = (event) ->
  switch event.data
    when 'speed_up' then screen_to_show(speed_up)
--- Secondary Screen Server POC.framer/server.js
+++ Secondary Screen Server POC.framer/server.js
@@ -1,22 +1,28 @@
+/*
+server.js
+A node/express HTTP server that forwards GET request parameters through to
+applications connected via WebSockets (ws)
+Jordan Stapinski - MHCI Harman Capstone CMU 2018
+*/
+
 'use strict';
 const express = require('express');
 const SocketServer = require('ws').Server;
-const path = require('path');
 const PORT = 3000;
-const INDEX = path.join(__dirname, 'index.html');
-//init Express
+// init Express server at localhost:3000
 var app = express();
 var server = app.listen(PORT, function () {
     console.log('node.js static server listening on port: ' + PORT +  ", with websockets listener")
 })
 
 const wss = new SocketServer({ server });
-//init Websocket ws and handle incoming connect requests
+// init Websocket ws and handle incoming connect requests
 wss.on('connection', function connection(ws) {
-    console.log("connection ...");
-    //General GET parser
+    console.log('New connection');
+    // General GET parser for HTTP Server (only once socket connection established)
     app.get('/:cmd', function (req, res){
-      let command = req.params.cmd
+      let command = req.params.cmd;
+      // Ignore icons being pushed through the HTTP Server
       if (command != 'favicon.ico') {
         console.log(command);
         res.send('confirmed, sending message ' + command + ' to the second screen.');
